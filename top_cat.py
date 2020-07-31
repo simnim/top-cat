3835fa6dfdf327f2a3f648fac7b1f0c7a121b930
Nick Hahner <nick.hahner@gmail.com>
2020-07-30 23:35:18 -0700

    pulling out useful parts of old code

--- top_cat.py
+++ top_cat.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
 """
-Nick Hahner 2017
+Nick Hahner 2017-2020
 Ever wanted to automate finding the top cute cat picture from /r/aww to send to your friends?
 All you need to do is run ./top_cat.py and your life will change forever.
 
-You'll need to create ~/.top_cat.json if you want to find dogs instead!
+You'll need to create ~/.top_cat/config.toml if you want to find dogs instead!
 You can also add your slack api key to ^ if you want a nifty slack integration.
-All items in the config file are optional.
+All items in the config file are optional. See example config for defaults.
+
+Usage:
+    top_cat.py [options]
+
+Options:
+    -h, --help            Show this help message and exit
+    -v, --verbose         Debug info
+    -c, --config          config file location [default: ~/.top_cat/config.toml]
+    -d, --db-file         sqlite3 db file location [default: ~/.top_cat/db]
+
 """
 
 import requests
 import re
 import base64
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
-import facebook
+# from googleapiclient import discovery
+# from oauth2client.client import GoogleCredentials
+# import facebook
 import sys
 import json
 import toml
@@ -23,92 +33,82 @@ import sqlite3
 import hashlib
 import os
 from time import sleep
-import StringIO
 from PIL import Image
-from io import BytesIO
+from io import BytesIO, StringIO
 from tempfile import NamedTemporaryFile
 
 # Because the reddit api links use escaped html strings ie &amp;
 from xml.sax.saxutils import unescape
 
 
-def get_config():
-    # Start off with default config and override with our config
-    this_script_dir = os.path.dirname(os.path.realpath(__file__))
-    default_config = toml.load(this_script_dir+'/top_cat_deault.toml')
+from docopt import docopt
 
-    CONFIG_FILE_LOC = os.path.expanduser("~/.top_cat.toml")
+THIS_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+DEFAULT_CONFIG = toml.load(this_script_dir+'/top_cat_default.toml')
+
+
+def get_config():
+    user_config_file_loc = os.path.expanduser("~/.top_cat/config.toml")
     # Let's query the config file
-    if os.path.isfile(CONFIG_FILE_LOC):
+    if os.path.isfile(user_config_file_loc):
         try:
-            top_cat_user_config = toml.load(CONFIG_FILE_LOC)
+            top_cat_user_config = toml.load(user_config_file_loc)
         except Exception as e:
-            print( "Malformed config file at '%s'" % (CONFIG_FILE_LOC) , file=sys.stderr)
+            print( "Malformed config file at '%s'" % (config_file_loc) , file=sys.stderr)
             exit(1)
     else:
         top_cat_user_config = dict()
 
-    top_cat_config = {**default_config, **top_cat_user_config}
+    top_cat_config = {**DEFAULT_CONFIG, **top_cat_user_config}
 
     # If we plan on posting to social media, let's make sure we have tokens to try
-    assert ( not POST_TO_SLACK_TF
-                or (POST_TO_SLACK_TF and SLACK_API_TOKEN and SLACK_API_TOKEN != 'YOUR__SLACK__API_TOKEN_GOES_HERE')
+    assert ( not top_cat_config['POST_TO_SLACK_TF']
+                or (top_cat_config['POST_TO_SLACK_TF'] and top_cat_config['SLACK_API_TOKEN'] and top_cat_config['SLACK_API_TOKEN'] != 'YOUR__SLACK__API_TOKEN_GOES_HERE')
             ), "If you want to post to slack then you need to add an api key to the config file!"
-    assert ( not POST_TO_FB_TF
-                or (POST_TO_FB_TF and FB_PAGE_ACCESS_TOKEN and FB_PAGE_ACCESS_TOKEN != 'YOUR__FB__PAGE_ACCESS_TOKEN_GOES_HERE')
+    assert ( not top_cat_config['POST_TO_FB_TF']
+                or (top_cat_config['POST_TO_FB_TF'] and top_cat_config['FB_PAGE_ACCESS_TOKEN'] and top_cat_config['FB_PAGE_ACCESS_TOKEN'] != 'YOUR__FB__PAGE_ACCESS_TOKEN_GOES_HERE')
             ), "If you want to post to FB then you need to add a fb page_access_token to the config"
     return top_cat_config
 
 
-# Get google vision api credentials
-credentials = GoogleCredentials.get_application_default()
-service = discovery.build('vision', 'v1', credentials=credentials)
-
-# Connect to the db. It creates the file if necessary.
-conn = sqlite3.connect(os.path.expanduser("~/.top_cat.db"))
-cur = conn.cursor()
-# Create the top_cat table and index
-[ cur.execute(sql) for sql in
-    [   """
-        CREATE TABLE IF NOT EXISTS
-            image (
-                image_id      INTEGER PRIMARY KEY,
-                timestamp_ins text not null default current_timestamp,
-                url           text not null,
-                file_hash     text not null,
-                title         text not null,
-                top_label     text not null
-            );
-        """
-    ,
-        """
-        CREATE TABLE IF NOT EXISTS
-            image_labels (
-                image_id      int not null,
-                label         text not null,
-                score         REAL,
-                FOREIGN KEY(image_id) REFERENCES image(image_id)
-            );
-        """
-    ,
-        """
-        CREATE INDEX IF NOT EXISTS
-            image_file_hash_index
-            on  image (
-                    file_hash
+def guarantee_tables_exist(db_conn):
+    # Get a local cursor
+    db_cur = db_conn.cursor()
+    # Create the top_cat table and index
+    for sql in """
+            CREATE TABLE IF NOT EXISTS
+                image (
+                    image_id      INTEGER PRIMARY KEY,
+                    timestamp_ins text not null default current_timestamp,
+                    url           text not null,
+                    file_hash     text not null,
+                    title         text not null,
+                    top_label     text not null
                 );
-        """
-    ,
-        """
-        CREATE INDEX IF NOT EXISTS
-            image_url_index
-            on  image (
-                    url
-                );
-        """
 
-    ]
-]
+            CREATE TABLE IF NOT EXISTS
+                image_labels (
+                    image_id      int not null,
+                    label         text not null,
+                    score         REAL,
+                    FOREIGN KEY(image_id) REFERENCES image(image_id)
+                );
+
+            CREATE INDEX IF NOT EXISTS
+                image_file_hash_index
+                on  image (
+                        file_hash
+                    );
+
+            CREATE INDEX IF NOT EXISTS
+                image_url_index
+                on  image (
+                        url
+                    );
+            """.split(';'):
+        db_cur.execute(sql.strip())
+    db_conn.commit()
+
 
 def get_thumb_content(img_content, image_max_size = (1000,1000)):
     pil_img = Image.open(StringIO.StringIO(img_content))
@@ -138,6 +138,7 @@ def fix_imgur_url(url):
             return url.replace('.gifv','.mp4')
     return url
 
+
 def fix_giphy_url(url):
     if 'gfycat.com' in url:
         # keep just the caPiTALIZed key and return a nice predictable url
@@ -154,10 +155,10 @@ def fix_url_in_dict(d):
         return fix_redd_url(fix_imgur_url(d['url']))
 
 
-def query_reddit_api(limit=10):
+def query_reddit_api(config, limit=10):
     # Try really hard to get reddit api results. Sometimes the reddit API gives back empty jsons.
-    for attempt in range(MAX_REDDIT_API_ATTEMPTS):
-        r = requests.get(f"https://www.reddit.com/r/aww/top.json?limit={limit}", headers={'User-Agent': 'linux:top-cat:v0.1'})
+    for attempt in range(config['MAX_REDDIT_API_ATTEMPTS']):
+        r = requests.get(f"https://www.reddit.com/r/aww/top.json?limit={limit}", headers={'User-Agent': 'linux:top-cat:v0.2.0'})
         j = r.json()
         if j.get("data") is not None:
             print( "Succesfully queried the reddit api after", attempt+1, "attempts" , file=sys.stderr)
@@ -180,124 +181,140 @@ def add_image_content_to_post_d(post):
     return post
 
 
-# links = [ i["data"]["url"] for i in j["data"]["children"]]
-# fixed_links = [ unescape(fix_imgur_url(u)) for u in links ]
-# links_map_to_title = dict(zip(fixed_links, [ unicode(i["data"]["title"]) for i in j["data"]["children"]]))
-
-# def is_jpg(url):
-#     return requests.get(url, stream=True).headers.get('content-type') == 'image/jpeg'
-
-# #just_jpgs = filter(is_jpg, fixed_links)
-# just_jpgs = [l for l in fixed_links if is_jpg(l)]
-
-
-reddit_response_json = query_reddit_api()
-
-
-# Make sure we have the images and labels stashed for any potentially new posts
-# Usually we just skip over a post since it's probably been in the top N for a few hours already
-for post_i, post in enumerate(reddit_response_json):
-    cur.execute('SELECT image_id FROM image WHERE url=?', (post['url'],))
-    image_found = cur.fetchone()
-    if not image_found:
-        # Did not find the url, must be a new post. Double check image table for existing hash
-        add_image_content_to_post_d(post)
-        cur.execute('SELECT image_id FROM image WHERE hash=?', (post['image_hash'],))
-        image_reposted = cur.fetchone()
-        if not image_reposted:
-            # Ok, it's truly an original post, let's cache the labels
-            #### FIXME: Add logic for caching labels
-
-# We're ready to figure out if the post has climbed up the ranks and become a top post
+def populate_labels_in_db_for_posts(reddit_response_json):
+    # Make sure we have the images and labels stashed for any potentially new posts
+    # Usually we just skip over a post since it's probably been in the top N for a few hours already
+    for post_i, post in enumerate(reddit_response_json):
+        db_cur.execute('SELECT image_id FROM image WHERE url=?', (post['url'],))
+        image_found = db_cur.fetchone()
+        if not image_found:
+            # Did not find the url, must be a new post. Double check image table for existing hash
+            add_image_content_to_post_d(post)
+            db_cur.execute('SELECT image_id FROM image WHERE hash=?', (post['image_hash'],))
+            image_reposted = db_cur.fetchone()
+            if not image_reposted:
+                # Ok, it's truly an original post, let's cache the labels
+                #### FIXME: Add logic for caching labels
 
 
-for post in reddit_response_json:
-    img_content = requests.get(img, stream=True).content
-    # Make sure it's small enough for the vision api
-    img_resized = get_thumb_content(img_content)
-    image_content_b64 = base64.b64encode(img_resized)
-    #Check if we already have the file in the db
-    retrieved_from_db = False
-    file_hash = hashlib.sha1(image_content_b64).hexdigest()
-    cur.execute('SELECT top_label FROM image WHERE file_hash=?', (file_hash,))
-    top_label = cur.fetchone()
-    if top_label:
-        #We've already got it.
-        top_label = top_label[0]
-        retrieved_from_db = True
-        print("IMAGE ALREADY IN DB:", top_label, img, unicode(links_map_to_title[img]))
-    else:
-        # Annotate image with google image api
-        service_request = service.images().annotate(body={
-            'requests': [{
-                'image': {
-                    'content': image_content_b64.decode('UTF-8')
-                },
-                'features': [{
-                    'type': 'LABEL_DETECTION',
-                    'maxResults': 10
-                }]
-            }]
-        })
-        response = service_request.execute()
-        assert  response.get('responses') and \
-                len(response['responses']) > 0 and\
-                response['responses'][0].get('labelAnnotations') \
-            , \
-                "Google vision api didn't seem to like the image... it returned no results. Wat do?"
-        labels_and_scores = [ (annot['description'], annot['score']) for annot in response['responses'][0]['labelAnnotations'] ]
-        top_label = labels_and_scores[0][0]
-        # Add the image to the db:
-        cur.execute("INSERT INTO image (url, file_hash, title, top_label) values (?,?,?,?)",
+def repost_to_social_media(post, label):
+    if top_cat_config['POST_TO_SLACK_TF']:
+        slack_payload = {
+            "token": top_cat_config['SLACK_API_TOKEN'],
+            "channel": top_cat_config['SLACK_CHANNEL'],
+            "text": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
+            "username": "TopCat",
+            "as_user": "TopCat",
+            "attachments": json.dumps([
+                    {
+                        "fallback": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
+                        "title": unicode(links_map_to_title[img]),
+                        "image_url": img
+                    }
+                ])
+        }
+        requests.get('https://slack.com/api/chat.postMessage', params=slack_payload)
+
+    if top_cat_config['POST_TO_FB_TF']:
+        fb_api = facebook.GraphAPI(top_cat_config['FB_PAGE_ACCESS_TOKEN'])
+        attachment =  {
+            'name': 'top_cat',
+            'link': img,
+            'picture': img
+        }
+        status = fb_api.put_wall_post(message=unicode(links_map_to_title[img]), attachment=attachment)
+
+
+
+def main():
+    # FIXME: Fill in completely
+
+    args = docopt(__doc__, version='0.2.0')
+
+    # DONTFIXME: Don't use vision api
+    # # Get google vision api credentials
+    # credentials = GoogleCredentials.get_application_default()
+    # service = discovery.build('vision', 'v1', credentials=credentials)
+
+    top_cat_config = get_config()
+
+    # Connect to the db. It creates the file if necessary.
+    db_conn = sqlite3.connect(os.path.expanduser(top_cat_config['DB_FILE']))
+    db_cur = db_conn.cursor()
+
+
+    ### FIXME: Clean up and refactor into more functions
+
+    # links = [ i["data"]["url"] for i in j["data"]["children"]]
+    # fixed_links = [ unescape(fix_imgur_url(u)) for u in links ]
+    # links_map_to_title = dict(zip(fixed_links, [ unicode(i["data"]["title"]) for i in j["data"]["children"]]))
+
+    # def is_jpg(url):
+    #     return requests.get(url, stream=True).headers.get('content-type') == 'image/jpeg'
+
+    # #just_jpgs = filter(is_jpg, fixed_links)
+    # just_jpgs = [l for l in fixed_links if is_jpg(l)]
+
+    reddit_response_json = query_reddit_api(top_cat_config)
+
+    populate_labels_in_db_for_posts(reddit_response_json)
+
+    # We're ready to figure out if the post has climbed up the ranks and become a top post
+    for label_to_search_for in top_cat_config['LABELS_TO_SEARCH_FOR']:
+        # Iterate down the list of reddit posts and see if there's a label_to_search_for for the post.
+        # FIXME: this part
+        db_cur.execute("INSERT INTO image (url, file_hash, title, top_label) values (?,?,?,?)",
                     (img, file_hash, unicode(links_map_to_title[img]), top_label))
-        conn.commit()
-        # Now get back the image_id of what we just inserted...
-        cur.execute('SELECT image_id FROM image WHERE file_hash=?', (file_hash,))
-        image_id = cur.fetchone()[0]
-
+        db_conn.commit()
+        db_cur.execute('SELECT image_id FROM image WHERE file_hash=?', (file_hash,))
+        image_id = db_cur.fetchone()[0]
         # Print out each label and label's score. Also store each result in the db.)
         print( "Labels for " + img + ':' , file=sys.stderr)
         for label, score in labels_and_scores:
             print( '    ' + label + ' = ' + str(score) , file=sys.stderr)
-            cur.execute("INSERT INTO image_labels (image_id, label, score) values (?,?,?)",
+            db_cur.execute("INSERT INTO image_labels (image_id, label, score) values (?,?,?)",
                         (image_id, label, score))
-            conn.commit()
+            db_conn.commit()
+
+    ### FIXME: Old code
+    for post in reddit_response_json:
+        img_content = requests.get(img, stream=True).content
+        # Make sure it's small enough for the vision api
+        img_resized = get_thumb_content(img_content)
+        image_content_b64 = base64.b64encode(img_resized)
+        #Check if we already have the file in the db
+        retrieved_from_db = False
+        file_hash = hashlib.sha1(image_content_b64).hexdigest()
+        db_cur.execute('SELECT top_label FROM image WHERE file_hash=?', (file_hash,))
+        top_label = db_cur.fetchone()
+        if top_label:
+            #We've already got it.
+            top_label = top_label[0]
+            retrieved_from_db = True
+            print("IMAGE ALREADY IN DB:", top_label, img, unicode(links_map_to_title[img]))
 
 
-    if top_label == LABEL_TO_SEARCH_FOR:
-        print("TOP %s FOUND!" % (LABEL_TO_SEARCH_FOR.upper()))
-        print("Titled:", unicode(links_map_to_title[img]))
-        print(img)
-        if not retrieved_from_db :
-            if POST_TO_SLACK_TF:
-                slack_payload = {
-                    "token": SLACK_API_TOKEN,
-                    "channel": SLACK_CHANNEL,
-                    "text": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
-                    "username": "TopCat",
-                    "as_user": "TopCat",
-                    "attachments": json.dumps([
-                            {
-                                "fallback": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
-                                "title": unicode(links_map_to_title[img]),
-                                "image_url": img
-                            }
-                        ])
-                }
-                requests.get('https://slack.com/api/chat.postMessage', params=slack_payload)
+        if top_label == LABEL_TO_SEARCH_FOR:
+            print("TOP %s FOUND!" % (LABEL_TO_SEARCH_FOR.upper()))
+            print("Titled:", unicode(links_map_to_title[img]))
+            print(img)
+            if not retrieved_from_db :
+                repost_to_social_media()
+            # We found the top cat, no need to keep going through images
+            break
 
-            if POST_TO_FB_TF:
-                fb_api = facebook.GraphAPI(top_cat_config['FB_PAGE_ACCESS_TOKEN'])
-                attachment =  {
-                    'name': 'top_cat',
-                    'link': img,
-                    'picture': img
-                }
-                status = fb_api.put_wall_post(message=unicode(links_map_to_title[img]), attachment=attachment)
+    if top_label != LABEL_TO_SEARCH_FOR:
+        # WHAT?!?!?!?! No cats???
+        print("WARNING: The internet is broken. No top_cat found...")
 
-        # We found the top cat, no need to keep going through images
-        break
+    ### /FIXME: Old code
 
-if top_label != LABEL_TO_SEARCH_FOR:
-    # WHAT?!?!?!?! No cats???
-    print("WARNING: The internet is broken. No top_cat found...")
+    ### /FIXME: Clean up and refactor into more functions
+
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
