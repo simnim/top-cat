3cc8fdde51d7c56cd1752d1e6991ad31797f5078
Nick Hahner <nick.hahner@gmail.com>
2020-08-08 20:22:08 -0700

    Working now for top_cat and top_dog.

--- top_cat.py
+++ top_cat.py
@@ -20,7 +20,7 @@ Options:
 
 """
 
-
+import time
 import requests
 import re
 import base64
@@ -40,6 +40,9 @@ import pyjq
 # from xml.sax.saxutils import unescape
 from docopt import docopt
 import tensorflow as tf
+# Turn off useless TF messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'; tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
 from deeplab import DeepLabModel
 from collections import Counter
 
@@ -50,11 +53,12 @@ from matplotlib import pyplot as plt
 from PIL import Image
 import string
 import random
+import pprint
 
 MAX_IMS_PER_VIDEO = 20
 
 # 10% is a solid cutoff...
-SCORE_CUTOFF = .1
+SCORE_CUTOFF = .05
 
 import mimetypes
 
@@ -100,7 +104,7 @@ def guarantee_tables_exist(db_conn):
                     post_id       INTEGER PRIMARY KEY,
                     timestamp_ins text not null default current_timestamp,
                     url           text not null,
-                    file_hash     text not null,
+                    media_hash    text not null,
                     title         text not null
                 );
 
@@ -113,10 +117,18 @@ def guarantee_tables_exist(db_conn):
                     FOREIGN KEY(post_id) REFERENCES post(post_id)
                 );
 
+            CREATE TABLE IF NOT EXISTS
+                top_post (
+                    top_post_id   INTEGER PRIMARY KEY,
+                    post_id       int not null,
+                    timestamp_ins text not null default current_timestamp,
+                    FOREIGN KEY(post_id) REFERENCES post(post_id)
+                );
+
             CREATE INDEX IF NOT EXISTS
-                media_file_hash_index
+                media_media_hash_index
                 on  post (
-                        file_hash
+                        media_hash
                     );
 
             CREATE INDEX IF NOT EXISTS
@@ -124,6 +136,13 @@ def guarantee_tables_exist(db_conn):
                 on  post (
                         url
                     );
+
+            CREATE INDEX IF NOT EXISTS
+                top_post_post_id_index
+                on  top_post (
+                        post_id
+                    );
+
             """.split(';'):
         db_cur.execute(sql.strip())
     db_conn.commit()
@@ -158,8 +177,16 @@ def fix_giphy_url(url):
 
 
 def fix_redd_url(url):
-    # Originally had it as 480.mp4 but that didn't always work... hopefully don't have to be more clever
-    return url+'/DASH_360.mp4' if 'v.redd.it' in url else url
+    if 'v.redd.it' in url:
+        # Unfortunately we can't predict what quality levels are available beforehand
+        # Protip from https://www.joshmcarthur.com/til/2019/05/20/httpsvreddit-video-urls.html
+        vid_id = re.findall('v.redd.it/([A-Za-z0-9]+)',url)[0]
+        dash_playlist = requests.get(f'https://v.redd.it/{vid_id}/DASHPlaylist.mpd')
+        available_qs = re.findall(r'DASH_(\d+)\.mp4',dash_playlist.text)
+        best_q = sorted(available_qs, key=lambda x: -int(x))[0]
+        return f'https://v.redd.it/{vid_id}/DASH_{best_q}.mp4'
+    else:
+        return url
 
 
 def fix_url_in_dict(d):
@@ -190,7 +217,7 @@ def query_reddit_api(config, limit=10):
     return nice_jsons
 
 
-def add_image_content_to_post_d(post, model, temp_dir):
+def add_image_content_to_post_d(post, temp_dir):
     " Add the image data to our post dictionary. Don't bother if it's already there. "
     if post.get('media_file') is None:
         #FIXME: Make this write to a tempfile instead
@@ -198,7 +225,7 @@ def add_image_content_to_post_d(post, model, temp_dir):
         post['media_file'] = temp_fname
         open(temp_fname,'wb').write(requests.get(post['url'], stream=True).content)
         post['media_hash'] = hashlib.sha1(open(temp_fname,'rb').read()).hexdigest()
-        add_labels_for_image_to_post_d(post,model)
+
 
 def add_labels_for_image_to_post_d(post, model):
     # FIXME
@@ -273,14 +300,15 @@ def populate_labels_in_db_for_posts(
     # Make sure we have the images and labels stashed for any potentially new posts
     # Usually we just skip over a post since it's probably been in the top N for a few hours already
     for post_i, post in enumerate(reddit_response_json):
-        db_cur.execute('SELECT post_id FROM post WHERE url=?', (post['url'],))
+        db_cur.execute('SELECT post_id, media_hash FROM post WHERE url=?', (post['url'],))
         image_found = db_cur.fetchone()
         if not image_found:
             # Did not find the url, must be a new post. (or maybe a repost...)
-            add_image_content_to_post_d(post,model,temp_dir)
+            add_image_content_to_post_d(post,temp_dir)
+            add_labels_for_image_to_post_d(post,model)
 
             #Check if we already have the file in the db
-            db_cur.execute("INSERT INTO post (url, file_hash, title) values (?,?,?)",
+            db_cur.execute("INSERT INTO post (url, media_hash, title) values (?,?,?)",
                               ( post['url'],
                                 post['media_hash'],
                                 post['title'] )
@@ -288,52 +316,91 @@ def populate_labels_in_db_for_posts(
             db_conn.commit()
             db_cur.execute('SELECT post_id FROM post WHERE url=?', (post['url'],))
             post_id = db_cur.fetchone()[0]
+            post['post_id'] = post_id
 
             # Print out each label and label's score. Also store each result in the db.)
             if config['VERBOSE']:
-                print("Labels for", post['title'], ':', file=sys.stderr)
+                print("Labels for", file=sys.stderr)
+                print(post['title'], ':', post['url'], file=sys.stderr)
             for label, score in zip(post['labels'],post['scores']):
                 # Sometimes a few pixels get ridiculous labels... 10% of the pixels having a label seems like a decent cutoff...
                 ignore_this_label = (label == 'background' or score < SCORE_CUTOFF)
                 if config['VERBOSE']:
-                    print('    ',label,'=',score, file=sys.stderr)
-                db_cur.execute("INSERT INTO post_label (post_id, label, score) values (?,?,?)",
-                            (post_id, label, score))
-                db_conn.commit()
+                    print('    ',label,'=',score, 'ignored' if ignore_this_label else '', file=sys.stderr)
+                if not ignore_this_label:
+                    db_cur.execute("INSERT INTO post_label (post_id, label, score) values (?,?,?)",
+                                (post_id, label, score))
+                    db_conn.commit()
+        else:
+            post['media_hash'] = image_found[1]
+            post['post_id'] = image_found[0]
+            # Fetch labels from db
+            db_cur.execute(" SELECT label, score FROM post_label where post_id=? order by score desc", (image_found[0],))
+            fetched_labels = db_cur.fetchall()
+            if fetched_labels:
+                post['labels'], post['scores'] = zip(*fetched_labels)
+            else:
+                post['labels'] = ['background']
+                post['scores'] = [1.0]
 
 
-def repost_to_social_media(post, label, top_cat_config):
+def repost_to_slack(post, label, top_cat_config):
     if top_cat_config['POST_TO_SLACK_TF']:
+        label_map_to_channel = dict(zip(top_cat_config['LABELS_TO_SEARCH_FOR'],
+                                        top_cat_config['SLACK_CHANNELS']))
         slack_payload = {
             "token": top_cat_config['SLACK_API_TOKEN'],
-            "channel": top_cat_config['SLACK_CHANNEL'],
-            "text": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
-            "username": "TopCat",
-            "as_user": "TopCat",
+            "channel": label_map_to_channel[label],
+            "text": f"Top {label} on /r/aww\n{post['url']}",
+            "username": f"Top{label.title()}",
+            "as_user": f"Top{label.title()}",
             "attachments": json.dumps([
                     {
-                        "fallback": "Top %s jpg on imgur (via /r/aww)" % (LABEL_TO_SEARCH_FOR),
-                        "title": unicode(links_map_to_title[img]),
-                        "image_url": img
+                        "fallback": f"Top {label} on /r/aww\n{post['url']}",
+                        "title": post['title'],
+                        # Currently this only works for images and gifs... :(
+                        "image_url": post['url']
                     }
                 ])
         }
         requests.get('https://slack.com/api/chat.postMessage', params=slack_payload)
+        if top_cat_config['VERBOSE']:
+            print('Posted',slack_payload)
 
-    if top_cat_config['POST_TO_FB_TF']:
-        fb_api = facebook.GraphAPI(top_cat_config['FB_PAGE_ACCESS_TOKEN'])
-        attachment =  {
-            'name': 'top_cat',
-            'link': img,
-            'picture': img
-        }
-        status = fb_api.put_wall_post(message=unicode(links_map_to_title[img]), attachment=attachment)
+# # CURRENTLY BROKEN. Facebook got rid of my access and won't give me a new one...
+# def repost_to_facebook(post, label, top_cat_config):
+#     if top_cat_config['POST_TO_FB_TF']:
+#         fb_api = facebook.GraphAPI(top_cat_config['FB_PAGE_ACCESS_TOKEN'])
+#         attachment =  {
+#             'name': 'top_cat',
+#             'link': img,
+#             'picture': img
+#         }
+#         status = fb_api.put_wall_post(message=unicode(links_map_to_title[img]), attachment=attachment)
 
 
 def update_config_with_args(config, args):
     # { 'DB_FILE': '--db-file' ... }
     args_keys_to_conig_keys = dict(zip(args.keys(),  [ a.strip('-').replace('-','_').upper() for a in args.keys()]))
     config.update([ (args_keys_to_conig_keys[argk], args[argk]) for argk in args.keys() if args[argk] is not None ])
+
+
+def maybe_repost_to_social_media(reddit_response_json, top_cat_config, db_conn):
+    db_cur = db_conn.cursor()
+    # We're ready to figure out if the post has climbed up the ranks and become a top post
+    # Only consider the first post... maybe do something fancier later.
+    top_post = reddit_response_json[0]
+    for label_to_search_for in top_cat_config['LABELS_TO_SEARCH_FOR']:
+        # Iterate down the list of reddit posts and see if there's a label_to_search_for for the post.
+        # FIXME: this part
+        if label_to_search_for in top_post['labels']:
+            db_cur.execute('SELECT * FROM top_post WHERE post_id=?', (top_post['post_id'],))
+            already_reposted = db_cur.fetchone()
+            if not already_reposted:
+                repost_to_slack(top_post,label_to_search_for,top_cat_config)
+                # repost_to_facebook(top_post,label_to_search_for,top_cat_config)
+                db_cur.execute("INSERT INTO top_post (post_id) values (?)", (top_post['post_id'],))
+                db_conn.commit()
 
 
 def main():
@@ -346,14 +413,6 @@ def main():
 
     top_cat_config = get_config(config_file_loc=args['--config'])
     update_config_with_args(top_cat_config, args)
-
-    # TF logging level based on configured logging level
-    if top_cat_config['VERBOSE']:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
-    else:
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
     # Connect to the db. Create the sqlite file if necessary.
     db_conn = sqlite3.connect(os.path.expanduser(top_cat_config['DB_FILE']))
@@ -370,7 +429,10 @@ def main():
     # What's new in /r/aww?
     reddit_response_json = query_reddit_api(top_cat_config)
 
-    # Label everything
+    # if top_cat_config['VERBOSE']: pprint.pp(reddit_response_json)
+
+    # Label everything... not really necessary since we could just label
+    #   the top_post but nice to have in the db regardless
     populate_labels_in_db_for_posts(
               reddit_response_json=reddit_response_json
             , model=model
@@ -379,17 +441,11 @@ def main():
             , config=top_cat_config
         )
 
-    # ### FIXME: Clean up and refactor into more functions
+    if top_cat_config['VERBOSE']:
+        pprint.pp(reddit_response_json)
 
-    # # We're ready to figure out if the post has climbed up the ranks and become a top post
-    # for label_to_search_for in top_cat_config['LABELS_TO_SEARCH_FOR']:
-    #     # Iterate down the list of reddit posts and see if there's a label_to_search_for for the post.
-    #     # FIXME: this part
-    #     if appropriate:
-    #         repost_to_social_media(post,label,top_cat_config)
+    maybe_repost_to_social_media(reddit_response_json, top_cat_config, db_conn)
 
-
-    # ### /FIXME: Clean up and refactor into more functions
 
 
 if __name__ == "__main__":
