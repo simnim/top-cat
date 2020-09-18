059ab76fadc3543f2431e1805872aefcbf60127d
Nick Hahner <nick.hahner@gmail.com>
2020-09-18 11:04:54 -0700

    Making this match

--- serve-top-posts.py
+++ serve-top-posts.py
@@ -8,7 +8,6 @@ import os
 # Get db conn ready
 from top_cat import get_config
 db_file_loc = os.path.expanduser(get_config()['DB_FILE'])
-print(db_file_loc)
 # We're just reading... so I think it's safe to share the connection on multiple threads
 conn = sqlite3.connect(db_file_loc, check_same_thread=False)
 conn.row_factory = sqlite3.Row
@@ -27,7 +26,7 @@ def index():
 @app.route('/top/<string:label>')
 def show_subpath(label):
     title = f"Top {label}"
-    posts = queries.get_top_posts(conn, label)
+    posts = queries.get_top_posts_for_flask(conn, label)
     # We need to know if the url is for a video or a picture!
     posts = [ {**post, 'type':mimetypes.guess_type(post['media'])[0].split('/')[0]} for post in posts]
     return render_template('top-post.html', title=title, posts=posts)
