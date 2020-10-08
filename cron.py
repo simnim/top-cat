c11814cd9465aaac702424cf8bb361b670712c85
Nick Hahner <nick.hahner@gmail.com>
2020-10-08 01:01:51 -0700

    pipefail

--- cron.py
+++ cron.py
@@ -3,17 +3,25 @@
 import os
 import sys
 import subprocess as sp
+from tempfile import NamedTemporaryFile
+
+top_cat_spam_f = NamedTemporaryFile()
 
 from top_cat import get_config, THIS_SCRIPT_DIR
 
-sp.call('mkdir -p $HOME/top_cat_logs/', shell=True)
+sp.call('mkdir -p $HOME/top_cat_logs/'
+    , shell=True
+)
 
-execution = sp.run(f'''{THIS_SCRIPT_DIR}/top_cat.py -v 2>&1 \
-                        | tee -a $HOME/top_cat_logs/$(date "+%Y-%m-%d")'''
+execution = sp.run(f'{THIS_SCRIPT_DIR}/top_cat.py -v 2>&1 > {top_cat_spam_f.name}'
                     , shell=True
-                    , stderr=sp.STDOUT
-                    , stdout=sp.PIPE
                 )
+
+output = sp.run(f'cat {top_cat_spam_f.name} | tee -a $HOME/top_cat_logs/$(date "+%Y-%m-%d")'
+                , shell=True
+                , stderr=sp.STDOUT
+                , stdout=sp.PIPE
+            )
 
 if execution.returncode:
     # Yell about it
@@ -24,7 +32,7 @@ if execution.returncode:
     slack_payload = {
         "token": get_config()['SLACK_API_TOKEN'],
         "channel": '#derps',
-        "text": str(execution.output),
+        "text": str(output.stdout),
         "username": "TopCatRunner",
         "as_user": "TopCatRunner",
     }
