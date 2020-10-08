2bc052fd84b2cca7818f49156387df3bdf404f63
Nick Hahner <nick.hahner@gmail.com>
2020-10-08 15:01:48 -0700

    python is better than bash for cron job

--- cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-# -l to make sure to source .bashrc for our google creds etc
-
-# Figure out where the top_cat script is
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# Run it from the correct relative location
-$SCRIPT_DIR/top_cat.py -v >> $HOME/top_cat_log 2>&1
